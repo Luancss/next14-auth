430a4a9353a4b5ed101f67464b0b5397b445a425
Luan <luan@okngroup.com.br>
2024-07-23 01:16:51 -0300

    add Poppins font and authentication UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@/components/ui/button"
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className
+          )}>
+        🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+        <Button variant="secondary" size="lg">
+          Sign in
+        </Button>
+      </div>
+    </main>
   );
 }
