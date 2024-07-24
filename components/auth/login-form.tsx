9681ad944091fc5a909ffa1ea4edb715e565caf0
Luan <luan@okngroup.com.br>
2024-07-24 01:03:02 -0300

    Form structure and validation schema using zod

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,30 @@
+"use client";
+
+import * as z from "zod";
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { CardWrapper } from "@/components/auth/card-wrapper"
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -8,7 +32,16 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form!
+      <Form {...form}>
+        <form 
+          onSubmit={form.handleSubmit(() => {})}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   )
 }
