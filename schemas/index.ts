ddce18e5e6fa49f5c072f128b824366a2cf10fd1
Luan <luan@okngroup.com.br>
2024-07-24 01:26:32 -0300

    Implement form fields and validation messages with zod schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,10 @@
-me de
+import * as z from "zod"
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+})
