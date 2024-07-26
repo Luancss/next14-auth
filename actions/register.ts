9ff6029c5e8498d012df3f07ee6054e0b2478081
Luan <luan@okngroup.com.br>
2024-07-26 01:20:45 -0300

    Add user creation logic and bcrypt for password hashing

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,37 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedField = RegisterSchema.safeParse(values);
 
   if (!validatedField.success) {
-    return {error: "Invalid fields!"};
+    return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedField.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
