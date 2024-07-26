ae27d0983e7e75b78fd73f40bf0442ca08050102
Luan <luan@okngroup.com.br>
2024-07-26 01:52:56 -0300

    Implement GitHub structure authentication using NextAuth

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { auth } from "./auth";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("ROUTE: ", req.nextUrl.pathname);
+  console.log("IS LOGGEDIN: ", isLoggedIn);
+})
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
