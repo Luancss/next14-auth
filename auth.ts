cd7ad62e4d17c6aadcc5a1bf6516b63ff86ed8f9
Luan <luan@okngroup.com.br>
2024-07-28 19:10:02 -0300

    Enhance user authentication and session management with role handling

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { getUserById } from "./data/user";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -11,13 +13,42 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    async signIn({ user }) {
+      if (!user.id) {
+        return false;
+      }
+
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser || !existingUser.emailVerified) {
+        return false;
+      }
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.user && session.user) {
+        session.user.role = token.role as UserRole;
+      }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
