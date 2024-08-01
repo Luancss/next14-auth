a65b64ecd5c350cde70a773b005f7b58f3238545
Luan <luan@okngroup.com.br>
2024-08-01 02:23:00 -0300

    Server-side email verification logic and token validation

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -3,15 +3,33 @@
 import { CardWrapper } from "./card-wrapper";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import {newVerification} from "@/actions/new-verification";
+import { useCallback, useEffect, useState } from "react";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch((error) => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -25,7 +43,9 @@ export const NewVerificationForm = () => {
       backButtonHref="/auth/login"
     >
       <div className="w-full flex justify-center items-center">
-        <BeatLoader />
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
