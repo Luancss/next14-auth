25478f469fc6d9758601d3c4bae5c948c0f877c7
Luan <luan@okngroup.com.br>
2024-08-04 04:00:27 -0300

    Implement Navbar with dynamic button highlighting and ProtectedLayout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { useSession, signOut } from "next-auth/react";
 
 const SettingsPage = () => {
-  const session = useSession();
+  const user = useCurrentUser();
 
   const onClick = () => {
    logout();
   };
 
   return (
-    <div>
-      {JSON.stringify(session)}
+    <div className="bg-white p-10 rounded-xl">
       <form>
         <button onClick={onClick} type="submit">
           Sign out
