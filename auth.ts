4084d3757e648a74745a2a2581874c63d7fb5d35
Luan <luan@okngroup.com.br>
2024-08-06 00:54:02 -0300

    Implement user authentication and UI component for user info display

--- auth.ts
+++ auth.ts
@@ -57,9 +57,14 @@ export const {
         session.user.id = token.sub;
       }
 
-      if (token.user && session.user) {
+      if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean; 
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -70,6 +75,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
