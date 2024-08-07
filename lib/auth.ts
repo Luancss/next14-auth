026a928aa343c1288de9e05876d17a9d4e0900f4
Luan <luan@okngroup.com.br>
2024-08-06 23:02:59 -0300

    Implement admin page with role-based access control

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,5 +2,13 @@ import { auth } from "@/auth";
 
 export const currentUser = async () => {
   const session = await auth();
+
   return session?.user;
 };
+
+export const currentRole = async () => {
+  const session = await auth();
+  
+  return session?.user?.role;
+};
+
--- routes.ts
+++ routes.ts
@@ -9,6 +9,7 @@ export const publicRoutes = [
   "/settings",
   "/server",
   "/client",
+  "/admin"
   // TOOD: Change public route settings
 ];
 
